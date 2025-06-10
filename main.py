5d91eeaf06541e60a99278f909b4371c6274dff4
sipps1 <siphovund@gmail.com>
2025-06-10 23:47:40 +0200

    fixed main

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# Mount static files
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+@app.get("/questions")
+def get_questions():
+    import json
+    with open("data/enriched_questions.json") as f:
+        data = json.load(f)
+    return {"questions": data}
