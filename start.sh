b40377453f00bd17750265a405cd34ab5b3ec825
sipps1 <siphovund@gmail.com>
2025-06-11 00:00:29 +0200

    added uvicorn to requirements

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+# Compare this snippet from .gitignore:
+# __pycache__/
+# .DS_Store
+# .env
+# .vscode/              # Visual Studio Code settings                   
